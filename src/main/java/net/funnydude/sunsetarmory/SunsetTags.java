e2b039945db7356d58eb0290e29799b95eff1055
norfolkzar <norfolkzar@gmail.com>
2025-11-24 21:09:45 +0530

    Merged v7.2

--- src/main/java/net/funnydude/sunsetarmory/SunsetTags.java
+++ src/main/java/net/funnydude/sunsetarmory/SunsetTags.java
@@ -9,6 +9,6 @@ import net.minecraft.world.item.Item;
 
 public class SunsetTags {
     public static final TagKey<EntityType<?>> SUNSET_ORDER = TagKey.create(Registries.ENTITY_TYPE, ResourceLocation.fromNamespaceAndPath(SunsetArmory.MODID, "sunset_order"));
-    public static final TagKey<Item> KINETIC_FOCUS = ItemTags.create(ResourceLocation.fromNamespaceAndPath(SunsetArmory.MODID, "kinetic_focus"));
+    public static final TagKey<Item> KINETIC_FOCUS = ItemTags.create((ResourceLocation.fromNamespaceAndPath(SunsetArmory.MODID,"kinetic_focus")));
 
 }
